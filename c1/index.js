fa64b609f4358152c8a9b1810ff680c0af9989b4
Sikof <32159109+Sikof@users.noreply.github.com>
2017-11-30 01:20:43 +0100

    ¯\_(ツ)_/¯

---
+++ c1/index.js
@@ -0,0 +1,26 @@
+console.log("Hellow world!");
+
+var ime = "Viktor"; // string (niza od karakteri)
+var godini =35;  // number
+var vistina = 182.5; //number 
+var temperatura = -22.5; //number
+var classEnded = true; //booLean
+
+
+console.log(ime);
+
+console.log(godini);
+
+console.log(vistina);
+
+console.log(temperatura);
+
+console.log(classEnded);
+
+
+godinaNaRagjanje = 1982; //camel case style
+
+godina_na_ragjanje = 1982; //snake case style
+
+console.log(ime + ' ' + prezime);  //a-z A-Z - $
+
